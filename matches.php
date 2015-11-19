78e6f7774e94e2e714e8b3fa1b785a13ae888b27
SRISAIYEEGHARAN <100680347@student.swin.edu.au>
2015-11-20 02:11:23 +1100

    added nav php

--- matches.php
+++ matches.php
@@ -24,18 +24,8 @@
 	</head>
 	<body>
 
-	<!-- Header -->
-			<header id="header">
-				<h1><a href="index.html">TarneitCricketClub</a></h1>
-				<nav id="nav">
-					<ul>
-						<li><a href="index.html">Home</a></li>
-						<li><a href="generic.html">Generic</a></li>
-						<li><a href="elements.html">Elements</a></li>
-						<li><a href="#" class="button special">Sign In</a></li>
-					</ul>
-				</nav>
-			</header>
+<!-- Navigation -->
+			<?php require 'nav.php'; ?>
 
 		<!-- Main -->
 			<section id="main" class="wrapper">
