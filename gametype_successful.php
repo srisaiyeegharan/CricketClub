cccb75a717e848751a7d1a8499b9b260a41b7605
SRISAIYEEGHARAN <100680347@student.swin.edu.au>
2015-11-20 14:18:43 +1100

    added game type successful page

--- gametype_successful.php
+++ gametype_successful.php
@@ -7,7 +7,7 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>TarneitCricketClub - Store a Game Type</title>
+		<title>TarneitCricketClub - Game Type</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
@@ -24,43 +24,28 @@
 	</head>
 	<body>
 
-	<!-- Navigation -->
+		<!-- Navigation -->
 			<?php require 'nav.php'; ?>
-			
+
 		<!-- Main -->
 			<section id="main" class="wrapper">
 				<div class="container">
 
 					<header class="major">
-						<h2>Store a Game Type</h2>
+						<h2>Game Type has been succesfully created</h2>
+						
 					</header>
+
+					<div class="container 25%">
+						<a class="button fit" href="admin.php">Club Management</a>
+					</div>
 				</div>
 			</section>
-			
-		<!-- Form -->
-						<section>
-							<form method="post" action="gametype_successful.php">
-							<div class="container 100%">
-								<div class="row uniform 100%">
-									<div class="6u 12u$">
-										<input type="text" name="game_type_id" id="game_type_id" value="" placeholder="Game Type ID" readonly="readonly" />
-									</div>
-									<div class="6u 12u$">
-										<input type="text" name="game_type_name" id="game_type_name" value="" placeholder="Game Type Name" />
-									</div>				
-									<div class="12u$">
-										<ul class="actions">
-											<li><input type="submit" value="Register"  /></li>
-											<li><input type="reset" value="Reset" class="special" /></li>
-										</ul>
-									</div>
-								</div>
-							</div>
-							</form>
-						</section>
 
 		<!-- Footer -->
-			<?php require 'footer.php'; ?>
+			<?php 
+				require 'footer.php'; 
+			?>
 
 	</body>
 </html>
