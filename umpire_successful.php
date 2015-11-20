b8688243c311408e096ca98f012e170027cca910
SRISAIYEEGHARAN <100680347@student.swin.edu.au>
2015-11-20 14:16:25 +1100

    added umpire successful page

---
+++ umpire_successful.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--
+	Transit by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>TarneitCricketClub - Umpire</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+	</head>
+	<body>
+
+		<!-- Navigation -->
+			<?php require 'nav.php'; ?>
+
+		<!-- Main -->
+			<section id="main" class="wrapper">
+				<div class="container">
+
+					<header class="major">
+						<h2>Umpire has been succesfully created</h2>
+						
+					</header>
+
+					<div class="container 25%">
+						<a class="button fit" href="admin.php">Club Management</a>
+					</div>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<?php 
+				require 'footer.php'; 
+			?>
+
+	</body>
+</html>
