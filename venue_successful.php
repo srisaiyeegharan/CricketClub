43228d261cd488962e850fa1700a53e8c851284d
SRISAIYEEGHARAN <100680347@student.swin.edu.au>
2015-11-20 14:09:33 +1100

    added venue successful page

--- venue_successful.php
+++ venue_successful.php
@@ -7,7 +7,7 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>TarneitCricketClub - Store a Venue</title>
+		<title>TarneitCricketClub - Venue</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
@@ -24,43 +24,28 @@
 	</head>
 	<body>
 
-	<!-- Navigation -->
+		<!-- Navigation -->
 			<?php require 'nav.php'; ?>
-			
+
 		<!-- Main -->
 			<section id="main" class="wrapper">
 				<div class="container">
 
 					<header class="major">
-						<h2>Store a Venue</h2>
+						<h2>Venue has been succesfully created</h2>
+						
 					</header>
+
+					<div class="container 25%">
+						<a class="button fit" href="admin.php">Club Management</a>
+					</div>
 				</div>
 			</section>
-			
-		<!-- Form -->
-						<section>
-							<form method="post" action="venue_successful.php">
-							<div class="container 100%">
-								<div class="row uniform 100%">
-									<div class="6u 12u$">
-										<input type="text" name="venue_id" id="venue_id" value="" placeholder="Venue ID" readonly="readonly" />
-									</div>
-									<div class="6u 12u$">
-										<input type="text" name="venue_name" id="venue_name" value="" placeholder="Venue Name" />
-									</div>				
-									<div class="12u$">
-										<ul class="actions">
-											<li><input type="submit" value="Register"  /></li>
-											<li><input type="reset" value="Reset" class="special" /></li>
-										</ul>
-									</div>
-								</div>
-							</div>
-							</form>
-						</section>
 
 		<!-- Footer -->
-			<?php require 'footer.php'; ?>
+			<?php 
+				require 'footer.php'; 
+			?>
 
 	</body>
 </html>
